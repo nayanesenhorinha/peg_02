ad64767ec77658ce7ef4d6101892fba20397003f
Nayane de Souza <159593131+nayanesenhorinha@users.noreply.github.com>
2024-05-08 19:28:23 -0300

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -1,21 +1,19 @@
--- Inserir dados na tabela Curso
-INSERT INTO escola.Curso (NomeCurso, Descricao, Duracao)
-VALUES 
-    ('Curso de Matemática', 'Curso introdutório de matemática', 6),
-    ('Curso de Inglês', 'Curso básico de inglês', 4);
+INSERT INTO escola.endereco(rua, numero, bairro, cidade, estado, cep) VALUES 
+('Rua A',12,'Goiaba','Rio de Janeiro','RJ','25636400'),
+('Rua B',17,'Limoeiro','Rio de Janeiro','RJ','25636583'),
+('Rua C',23,'Carmelia','Rio de Janeiro','RJ','25636253');
 
--- Inserir dados na tabela Turma
-INSERT INTO escola.Turma (CodigoTurma, Turno, Polo, DataInicio, DataTermino, ID_Curso)
-VALUES
-    ('MAT-01', 'Manhã', 'Polo A', '2024-06-01', '2024-12-01', 1),
-    ('ING-01', 'Tarde', 'Polo B', '2024-07-01', '2024-10-01', 2);
+INSERT INTO escola.Curso(NomeCurso, Descricao, Duracao) VALUES
+('Matemática','Curso básico de matemática',10),
+('Portugues','Curso básico de postuguês',20),
+('Filosofia','Curso básico de filosofia',8);
 
--- Inserir dados na tabela Módulo
-INSERT INTO escola.Modulo (Nome, Descricao, Horas, ID_Facilitador, ID_Curso)
-VALUES
-    ('Matemática Básica', 'Módulo introdutório de matemática', 20, 1, 1),
-    ('Álgebra Linear', 'Módulo sobre álgebra linear', 30, 1, 1),
-    ('Geometria', 'Módulo de geometria básica', 25, 1, 1),
-    ('Inglês para Iniciantes', 'Módulo introdutório de inglês', 15, 2, 2),
-    ('Gramática', 'Módulo sobre gramática inglesa', 20, 2, 2),
-    ('Conversação', 'Módulo para prática de conversação em inglês', 25, 2, 2);
+INSERT INTO escola.Pessoa(NomeCompleto, DataNascimento, Genero, Email, Telefone, ID_Endereco) VALUES
+('Maria Antônia','12-05-2002','F','maria@antonia.com',2196352-8432,1),
+('João Victor','10-04-1995','M','joao@victor.com',2192685-1234,2),
+('Paulo Henrique','22-12-2000','M','paulo@victor.com',2192375-7415,3);
+
+INSERT INTO escola.Estudante(NumeroMatricula, DataMatricula, ID_Pessoa) VALUES 
+(123,'07-05-2024',1),
+(456,'06-05-2024',2),
+(789,'05-05-2024',3);
